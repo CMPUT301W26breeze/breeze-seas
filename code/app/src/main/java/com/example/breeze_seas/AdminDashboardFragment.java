1fa3d2d54a7fc91b85e628b341f2de60c294e5da
ji-minjeong <guanghua@ualberta.ca>
2026-03-07 20:14:02 -0700

    Minor fix

--- code/app/src/main/java/com/example/breeze_seas/AdminDashboardFragment.java
+++ code/app/src/main/java/com/example/breeze_seas/AdminDashboardFragment.java
@@ -10,32 +10,28 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
+/**
+ * A fragment for the main dashboard for admins.
+ */
 public class AdminDashboardFragment extends Fragment {
-    // Using the modern constructor to automatically inflate the layout
     public AdminDashboardFragment() { super(R.layout.fragment_admin_dashboard); }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // 1. Setup the Toolbar back button
         MaterialToolbar toolbar = view.findViewById(R.id.ad_topAppBar);
         toolbar.setNavigationOnClickListener(v -> {
-            // This will pop the dashboard off and return to your main app flow (e.g., the Profile tab)
             requireActivity().getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, new ProfileFragment())
                     .commit();
         });
 
-        // 2. Find the "View All Events" button
         Button btnViewEvents = view.findViewById(R.id.ad_btn_view_events);
 
-        // 3. Set the click listener to route to the Events list
         btnViewEvents.setOnClickListener(v -> {
             requireActivity().getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, new AdminBrowseEventsFragment())
-                    // If you want the physical hardware back button on the phone to return to
-                    // this dashboard from the events list, uncomment the line below:
                     .addToBackStack(null)
                     .commit();
         });
